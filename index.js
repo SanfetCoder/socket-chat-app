8406082a82a550fac85b8f135797c1ac6efbe053
Yang Predator <newkidsth@gmail.com>
2024-03-13 15:33:24 +0700

    Able to retrieve chat message event

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
+// socket.io
+const { Server } = require("socket.io");
+// Mount socket.io server to HTTP server
+const io = new Server(server);
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/index.html')
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-server.listen(3000, ()=>{
-    console.log('listening on :3000')
-})
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  //   disconnect event
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  //   chat message event
+  socket.on("chat message", (msg)=>{
+    console.log("message: " + msg)
+  })
+});
+
+server.listen(3000, () => {
+  console.log("listening on :3000");
+});
